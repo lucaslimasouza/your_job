d1ba9afdcc51a1b1d338e24c7ca6be5b1abd05ce
Lucas Lima de Souza <lucas.lima@vizir.com.br>
2014-08-13 18:53:07 -0300

    Fixing like postgle

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
 
   def index
     query = params[:q]
-    @jobs = query.blank? ? Job.all : Job.where("title LIKE ? ", "%#{query}%")
+    @jobs = query.blank? ? Job.all : Job.where("title ILIKE ? ", "%#{query}%")
     respond_with @jobs
   end
 end
