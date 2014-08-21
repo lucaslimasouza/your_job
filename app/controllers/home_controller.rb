9786b20fe3a6975ba0cf4db9e07e480f56dbb2f5
Lucas Lima de Souza <lucas.lima@vizir.com.br>
2014-08-21 14:31:03 -0300

    Reparando busca e definição pt-BR

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
 
   def index
     query = params[:q]
-    @jobs = query.blank? ? Job.all : Job.where("title ILIKE ? ", "%#{query}%")
+    @jobs = query.blank? ? Job.all :  Job.joins(:city).where("cities.name ILIKE ? or title ILIKE ?", "%#{query}%","%#{query}%")
     respond_with @jobs
   end
 
